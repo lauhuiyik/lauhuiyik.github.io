904837d36d7a05f35546ff8582162443aaf0342d
Chan Guan Hao <guanhao3797@gmail.com>
2016-06-18 01:53:28 +0800

    build script and more

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import re
 from urlparse import urljoin
 
 import requests
@@ -10,6 +11,47 @@ BASE = 'http://ericlauhuiyik.weebly.com'
 
 def unique(li):
     return list(set(li))
+
+
+def apply_custom_changes(url, soup):
+    script_tag = soup.select('script[src*="slideshow-jq.js"]')
+    no_of_script_tag = len(script_tag)
+    if no_of_script_tag > 1:
+        raise Exception('Expected 1 slideshow-jq.js, got '
+                        + str(no_of_script_tag))
+    elif no_of_script_tag == 1:
+        script_tag[0]['src'] = '/slideshow-jq.js'  # use local version
+
+    # remove weebly sign up banners
+    signup_div = soup.find('div', {'id': 'weebly-footer-signup-container'})
+    signup_div.extract()
+
+    if url == '/':
+        # previously had extra whitespace
+        textarea_tag = soup.find('textarea')
+        textarea_tag.string = ''
+
+        anchor_tag = soup.select('form .wsite-button')
+        del anchor_tag[0]['onclick']
+        custom_script = soup.new_tag('script')
+        custom_script['src'] = '/formSubmit.js'
+        body_tag = soup.find('body')
+        body_tag.append(custom_script)
+
+        # make json output readable
+        input_tags = soup.find_all('input', {'type': 'text'})
+        input_tags[0]['name'] = 'name'
+        input_tags[1]['name'] = 'email'
+        radio_btns = soup.find_all('input', {'type': 'radio'})
+        for btn in radio_btns:
+            btn['name'] = 'motive'
+        textarea_tag['name'] = 'message'
+
+
+def apply_custom_output(url, html):
+    if url == '/':
+        return re.sub(r'>\s+</textarea>', '></textarea>', html)
+    return html
 
 
 def download_page(url, soup=None):
@@ -26,24 +68,20 @@ def download_page(url, soup=None):
     static_assets = [
         {
             'tag_name': 'img',
-            'check_for': [
-                {'src': True},
-            ],
+            'check_for': {'src': True},
             'attr': 'src',
         },
         {
             'tag_name': 'link',
-            'check_for': [
-                {'href': True},
-                {'rel': 'stylesheet'},
-            ],
+            'check_for': {
+                'href': True,
+                'rel': 'stylesheet',
+            },
             'attr': 'href',
         },
         {
             'tag_name': 'script',
-            'check_for': [
-                {'src': True},
-            ],
+            'check_for': {'src': True},
             'attr': 'src',
         }
     ]
@@ -53,8 +91,13 @@ def download_page(url, soup=None):
         tags = filter(lambda i: not i[attr].startswith('//'), tags)
         for tag in tags:
             tag[attr] = urljoin(BASE, tag[attr])
+
+    apply_custom_changes(url, soup)
     with open(filename, 'w+') as f:
-        f.write(soup.prettify().encode('utf-8'))
+        html = soup.prettify().encode('utf-8')
+        output = apply_custom_output(url, html)
+        f.write(output)
+        print filename
 
 
 def download_site():
---
+++ formSubmit.js
@@ -0,0 +1,22 @@
+var webhook = 'https://hooks.zapier.com/hooks/catch/1443994/4be9du/';
+
+function arrayToObj(arr) {
+  var obj = {}
+  for (var i=0; i < arr.length; i++) {
+    obj[arr[i].name] = arr[i].value;
+  }
+  return obj;
+}
+
+var $form = $('form');
+
+$('form .wsite-button').click(function() {
+  var data = arrayToObj($('form').serializeArray());
+  $.post(webhook, data, function(res) {
+    $form.hide();
+    $form.parent().append('<span class="success-msg">Awesome! Will get back to you soon</span>');
+  })
+  .fail(function() {
+    $form.prepend('<span class="fail-msg">Oops, something went wrong.');
+  });
+});
